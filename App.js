9d3d744260c350f505e7968e11599ef49e22faa3
Fred Jiang <jiangf@shinetechsoftware.com>
2022-12-06 20:53:30 +0800

    update

--- App.js
+++ App.js
@@ -1,16 +1,46 @@
+import { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet } from 'react-native';
+import { Alert, StyleSheet } from 'react-native';
 import BottomTabNavigator from './app/navigator/BottomTabNavigator';
 import * as Sentry from 'sentry-expo';
+import { AppState } from 'react-native';
+import * as Updates from 'expo-updates';
 
 Sentry.init({
   dsn: 'https://8dba2a9aef96488e8ccde6c3894f960d@o4503904847724544.ingest.sentry.io/4504278435495936',
-  // enableInExpoDevelopment: true,
+  enableInExpoDevelopment: true,
   debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
 });
 
 export default function App() {
-  Sentry.Native.captureException(new Error('App Run'));
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', (appState) => {
+      if (appState === 'active') {
+        checkUpdate();
+      }
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  const checkUpdate = async () => {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        Alert.alert('Apply update', 'Hey there! We got an update for you 🥳🎉.', [
+          {
+            text: 'Cancel'
+          },
+          { text: 'OK', onPress: () => Updates.reloadAsync() },
+        ]);
+      }
+    } catch (e) {
+      // handle or log error
+    }
+  };
 
   return (
     <NavigationContainer>
--- app/screen/Time/Time.jsx
+++ app/screen/Time/Time.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, Text } from 'react-native';
+import * as Sentry from 'sentry-expo';
 
 export default function TimeScreen() {
-  return <Text>Time 1234567</Text>;
+  const throwError = () => {
+    Sentry.Native.captureException(new Error('This is small time error'));
+    alert('capture sentry error');
+  };
+
+  return (
+    <>
+      <Text>Time 1234</Text>
+      <Button title="Throw Sentry Error" onPress={throwError} />
+    </>
+  );
 }
