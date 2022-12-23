272d6b2770dfd61c226f1ad68b4b29655bd70254
Fred Jiang <jiangf@shinetechsoftware.com>
2022-12-23 13:49:13 +0800

    copy

--- app/screen/Time/TimeScreen.jsx
+++ app/screen/Time/TimeScreen.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { Button, Text } from 'react-native';
+import { Button, Pressable, Text } from 'react-native';
 import { useNotifications } from '../../hooks/useNotifications';
+import * as Clipboard from 'expo-clipboard';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function TimeScreen() {
   const { token, sendLocalNotification } = useNotifications();
+
+  const handleCopyStatRecorderCode = async () => {
+    await Clipboard.setStringAsync(token);
+    alert('copy token');
+  };
 
   return (
     <>
       <Text>Time 1234</Text>
       <Text>token: {token}</Text>
-      <Button title="Local Notification" onPress={() => sendLocalNotification()} />
+      <Pressable onPress={handleCopyStatRecorderCode}>
+        <MaterialCommunityIcons name="content-copy" size={28} />
+      </Pressable>
+      <Button
+        title="Local Notification"
+        onPress={() => sendLocalNotification()}
+      />
     </>
   );
 }
