cc876d9b4170fe44b044ec8276bb1b06818c30a1
Fred Jiang <jiangf@shinetechsoftware.com>
2022-12-23 12:47:35 +0800

    show token

--- app/screen/Time/TimeScreen.jsx
+++ app/screen/Time/TimeScreen.jsx
@@ -3,11 +3,12 @@ import { Button, Text } from 'react-native';
 import { useNotifications } from '../../hooks/useNotifications';
 
 export default function TimeScreen() {
-  const { sendLocalNotification } = useNotifications();
+  const { token, sendLocalNotification } = useNotifications();
 
   return (
     <>
       <Text>Time 1234</Text>
+      <Text>token: {token}</Text>
       <Button title="Local Notification" onPress={() => sendLocalNotification()} />
     </>
   );
