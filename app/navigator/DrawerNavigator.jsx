8efa5e0efce712c2a96f9433be4c9c60318654d8
Fred Jiang <jiangf@shinetechsoftware.com>
2023-01-04 14:03:51 +0800

    add drawer

--- app/navigator/DrawerNavigator.jsx
+++ app/navigator/DrawerNavigator.jsx
@@ -1,31 +1,55 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+} from '@react-navigation/drawer';
 import BottomTabNavigator from './BottomTabNavigator';
+import SettingsScreen from '../screen/SettingsScreen';
+import { useNavigation, useNavigationState } from '@react-navigation/native';
+import { Pressable } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Drawer = createDrawerNavigator();
-const Settings = () => <Text>Settings</Text>;
-
 const DrawerContent = ({ navigation }) => {
   return (
-      <DrawerContentScrollView>
-        <DrawerItem label="Settings" onPress={() => navigation.navigate('Settings')} />
-      </DrawerContentScrollView>
+    <DrawerContentScrollView>
+      <DrawerItem
+        label="Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
+    </DrawerContentScrollView>
   );
 };
 
 const DrawerNavigator = () => {
+  const state = useNavigationState((state) => state);
+  const isMainScreen = !state || state.routeNames[state.index] === 'BottomTab';
+  const navigation = useNavigation();
+
   return (
     <Drawer.Navigator
+      drawerContent={(props) => <DrawerContent {...props} />}
       screenOptions={{
         drawerPosition: 'right',
         drawerType: 'front',
         swipeEnabled: false,
-        headerShown: false
+        headerShown: !isMainScreen,
+        headerLeft: () => (
+          <Pressable onPress={() => navigation.goBack()}>
+            <MaterialCommunityIcons
+              name="chevron-left"
+              size={32}
+              style={{
+                marginLeft: 8,
+              }}
+            />
+          </Pressable>
+        ),
       }}
     >
       <Drawer.Screen name="BottomTab" component={BottomTabNavigator} />
-      <Drawer.Screen name="Settings" component={Settings} />
+      <Drawer.Screen name="Settings" component={SettingsScreen} />
     </Drawer.Navigator>
   );
 };
--- app/navigator/MomentsNavigator.jsx
+++ app/navigator/MomentsNavigator.jsx
@@ -1,13 +1,25 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Pressable, Text } from 'react-native';
 import MomentsScreen from '../screen/Moments/MomentsScreen';
 import SendMomentScreen from '../screen/Moments/SendMomentScreen';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 
-export default function MomentsNavigator() {
+export default function MomentsNavigator({ navigation }) {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Moments" component={MomentsScreen} />
+      <Stack.Screen
+        name="Moments"
+        component={MomentsScreen}
+        options={{
+          headerRight: () => (
+            <Pressable onPress={() => navigation.openDrawer()}>
+              <MaterialCommunityIcons name='menu' size={24}/>
+            </Pressable>
+          ),
+        }}
+      />
       <Stack.Screen name="SendMoment" component={SendMomentScreen} />
     </Stack.Navigator>
   );
