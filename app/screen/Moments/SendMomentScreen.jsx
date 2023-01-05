0aa254805edb58a0ce9c79383e5644a86f780321
Fred Jiang <jiangf@shinetechsoftware.com>
2023-01-05 09:56:17 +0800

    add form

--- app/screen/Moments/SendMomentScreen.jsx
+++ app/screen/Moments/SendMomentScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   Button,
   Text,
@@ -13,36 +13,68 @@ import momentService from '../../api/momentService';
 import colors from '../../config/colors';
 import useImagePicker from '../../hooks/useImagePicker';
 import Icon from '../../components/Icon';
+import { Formik, useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  desc: Yup.string().required().label('Text'),
+});
+
+const initValue = {
+  desc: '',
+};
 
 export default function SendMomentScreen({ navigation, route }) {
-  const [text, setText] = useState('');
   const { openImagePickerAsync, openCameraAsync, image } = useImagePicker();
 
-  const handleSubmit = () => {
-    momentService.createMoment({ desc: text, imageUrl: image.uri });
+  // const { handleChange, handleSubmit, errors, setFieldTouched } = useFormik({
+  //   initialValues: initValue,
+  //   validationSchema: validationSchema,
+  //   onSubmit: (values) => handleSendMoment(values),
+  // });
+
+  const handleSendMoment = (data) => {
+    momentService.createMoment({ ...data, imageUrl: image.uri });
     navigation.goBack();
   };
 
   return (
     <ScrollView style={{ paddingLeft: 8, paddingRight: 8 }}>
-      <View style={styles.textInputContainer}>
-        <Text style={{ width: 50 }}>Text:</Text>
-        <TextInput onChangeText={setText} style={styles.textInput} />
-      </View>
+      <Formik
+        initialValues={initValue}
+        validationSchema={validationSchema}
+        onSubmit={(values) => handleSendMoment(values)}
+      >
+        {({ handleChange, handleSubmit, errors, setFieldTouched }) => (
+          <>
+            <View style={styles.textInputContainer}>
+              <Text style={{ width: 50 }}>Text:</Text>
+              <TextInput
+                name="desc"
+                placeholder="Say something...."
+                onBlur={() => setFieldTouched('desc')}
+                onChangeText={handleChange('desc')}
+                style={styles.textInput}
+              />
+            </View>
+            <Text style={{ color: 'red' }}>{errors['desc']}</Text>
 
-      {image.uri && (
-        <Image style={styles.thumbnail} source={{ uri: image.uri }} />
-      )}
+            {image.uri && (
+              <Image style={styles.thumbnail} source={{ uri: image.uri }} />
+            )}
 
-      <View style={{ display: 'flex', flexDirection: 'row' }}>
-        <Pressable onPress={openCameraAsync}>
-          <Icon name="camera" />
-        </Pressable>
-        <Pressable onPress={openImagePickerAsync}>
-          <Icon name="file-image" />
-        </Pressable>
-      </View>
-      <Button title="Save" onPress={handleSubmit} />
+            <View style={{ display: 'flex', flexDirection: 'row' }}>
+              <Pressable onPress={openCameraAsync}>
+                <Icon name="camera" />
+              </Pressable>
+              <Pressable onPress={openImagePickerAsync}>
+                <Icon name="file-image" />
+              </Pressable>
+            </View>
+            <Button title="Save" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
     </ScrollView>
   );
 }
@@ -53,7 +85,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 16,
-    marginBottom: 8
+    marginBottom: 8,
   },
   textInput: {
     height: 40,
